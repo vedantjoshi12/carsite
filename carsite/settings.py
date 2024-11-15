d010fac540ead0a6bb458381f418f038e18c8fa9
Vedant Joshi <vedantvjoshi02@gmail.com>
2024-11-15 10:52:31 +0530

    Added/updated settings.py with deployment configurations

--- carsite/settings.py
+++ carsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure--wtw%-o+kw-#($9vx2$dqtyw+t1=o)l0oe=panlnod+hnrqquq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
